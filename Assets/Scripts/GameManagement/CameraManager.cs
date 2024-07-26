94d880623754f40b2a88f172ac920ece0f2c1e43
metalboroda <89025071+metalboroda@users.noreply.github.com>
2024-07-26 21:48:12 +0300

    Added Adaptive Ortho Size

--- Assets/Scripts/GameManagement/CameraManager.cs
+++ Assets/Scripts/GameManagement/CameraManager.cs
@@ -1,4 +1,4 @@
-using Cinemachine;
+﻿using Cinemachine;
 using UnityEngine;
 
 namespace Assets.Scripts.GameManagement
@@ -12,10 +12,35 @@ namespace Assets.Scripts.GameManagement
 
     private CinemachineConfiner _cinemachineConfiner;
 
+    private readonly float _baseResolutionHeight = 1920f;
+    private readonly float _baseOrthoSize = 9.25f;
+    private Vector2 _lastScreenSize;
+
     private void Awake() {
       Instance = this;
 
       _cinemachineConfiner = playerMainCamera.GetComponent<CinemachineConfiner>();
+
+      AdjustOrthoSize();
+    }
+
+    private void Start() {
+      _lastScreenSize = new Vector2(Screen.width, Screen.height);
+    }
+
+    private void Update() {
+      if (Screen.width != _lastScreenSize.x || Screen.height != _lastScreenSize.y) {
+        _lastScreenSize = new Vector2(Screen.width, Screen.height);
+
+        AdjustOrthoSize();
+      }
+    }
+
+    private void AdjustOrthoSize() {
+      float currentResolutionHeight = Screen.height;
+      float orthoSize = _baseOrthoSize * (currentResolutionHeight / _baseResolutionHeight);
+
+      playerMainCamera.m_Lens.OrthographicSize = orthoSize;
     }
 
     public void SetPlayerMainCamera(Transform target) {
